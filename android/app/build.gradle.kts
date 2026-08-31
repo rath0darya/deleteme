c0de7993932657927c603111c68a5f3c491b5a22
Rathod Aravind <187317337+rath0darya@users.noreply.github.com>
2026-08-31 17:05:53 +0530

    Clean Android dependencies and bump app version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,13 +8,14 @@ android {
         applicationId = "com.rath0darya.deleteme"
         minSdk = 23
         targetSdk = 37
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
     }
 }
 
 dependencies {
     implementation("androidx.core:core:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
     implementation("com.google.android.material:material:1.13.0")
 }
