44d21a453b0ae2b0945f7179189e62e98757c399
Rathod Aravind <187317337+rath0darya@users.noreply.github.com>
2026-08-31 12:33:07 +0530

    Add DeleteMe Android app module

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application") }
+
+android { namespace = "com.rath0darya.deleteme"; compileSdk = 37
+    defaultConfig { applicationId = "com.rath0darya.deleteme"; minSdk = 23; targetSdk = 37; versionCode = 1; versionName = "1.0" }
+}
